392d3e1ae2169b51484f124018768d52cf805b78
Jorge Soto <djsmoothzouk@gmail.com>
2021-02-03 00:20:41 -0600

    created basic components

---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Form = props => {
+    return (
+        <form>
+            <input
+            type="text"
+            name=""
+            placeholder="">
+            </input>
+        </form>
+    )
+}
+
+
+export default Form
